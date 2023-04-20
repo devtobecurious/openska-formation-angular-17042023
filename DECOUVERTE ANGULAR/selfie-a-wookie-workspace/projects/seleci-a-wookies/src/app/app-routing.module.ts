b2ac22cf3379a338d2ba7d71a0521ab5aaf34333
Evan BOISSONNOT <evan@boissonnot.fr>
2023-04-20 17:42:16 +0200

    feat: jrnee 4

--- DECOUVERTE ANGULAR/selfie-a-wookie-workspace/projects/seleci-a-wookies/src/app/app-routing.module.ts
+++ DECOUVERTE ANGULAR/selfie-a-wookie-workspace/projects/seleci-a-wookies/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPlanetsComponent } from './features/planets/list-planets/list-planets.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'planets/:id',
+
+  component: ListPlanetsComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- DECOUVERTE ANGULAR/selfie-a-wookie-workspace/projects/seleci-a-wookies/src/app/features/planets/list-planets/list-planets.component.ts
+++ DECOUVERTE ANGULAR/selfie-a-wookie-workspace/projects/seleci-a-wookies/src/app/features/planets/list-planets/list-planets.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { map, Observable, tap } from 'rxjs';
+import { LoggerService } from '../../../shared/tools/logger.service';
 import { ApiResult, Planet, Planets } from '../models';
+import { PlanetBusinessService } from '../services/planet-business.service';
 
 @Component({
   selector: 'saw-list-planets',
@@ -9,31 +12,24 @@ import { ApiResult, Planet, Planets } from '../models';
   styleUrls: ['./list-planets.component.css']
 })
 export class ListPlanetsComponent implements OnInit {
-  planets$: Observable<Planets>;
+  planets$ = inject(PlanetBusinessService).getAll();
+  route = inject(ActivatedRoute);
 
   /**
    *
    */
-  constructor(private readonly httpClient: HttpClient) {
-    this.planets$ = this.httpClient.get<ApiResult>('https://swapi.dev/api/planets/').pipe(
-      tap(item => console.log('avant', item)),
-      map((apiResult: ApiResult) => {
-        const tableauResult = apiResult.results.map(planet => {
-          return {
-            name: planet.name,
-            id: 1
-          };
-        });
-
-        return tableauResult;
-      }),
-      tap(item => console.log('apres', item)),
-    );
-  }
+  // constructor(private readonly httpClient: HttpClient,
+  //   private readonly logger: LoggerService) {
+  //   this.planets$ =
+  // }
   ngOnInit(): void {
-    this.planets$.subscribe(planets => {
-      this.planets = planets;
-    });
+    // this.route.params.subscribe(params => {
+    //   params['id']
+    // });
+    this.route.snapshot.params['id'];
+    // this.planets$.subscribe(planets => {
+    //   this.planets = planets;
+    // });
   }
 
   planets: Planets = [{
